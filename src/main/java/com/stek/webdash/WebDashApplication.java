a205def7aa0b171b484f46d9afa11ef74a40f7ce
Stefan Kajkut <stefan.kajkut@codecentric.de>
2020-04-09 01:16:01 +0200

    Project initialization

---
+++ src/main/java/com/stek/webdash/WebDashApplication.java
@@ -0,0 +1,13 @@
+package com.stek.webdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stek/webdash/WebDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stek.webdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
